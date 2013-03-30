5459d8f1537864f33276638e1d1a9b8b444e32b3
harsha.matadhikari@gmail.com <harsha.matadhikari@gmail.com@837e750e-98ea-f7b5-08b2-a96902323fd1>
2013-03-30 12:21:36 +0000

    PA3 Changes

--- trunk/prog3/src/cs553/pa3jsp/Pa3jspServlet.java
+++ trunk/prog3/src/cs553/pa3jsp/Pa3jspServlet.java
@@ -29,11 +29,15 @@ import java.util.Date;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.KeyFactory.Builder;
 
 @SuppressWarnings("serial")
 public class Pa3jspServlet extends HttpServlet {
 	public void doPost(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
+			throws ServletException, IOException  {
         UserService userService = UserServiceFactory.getUserService();
         User user = userService.getCurrentUser();
 
@@ -61,9 +65,11 @@ public class Pa3jspServlet extends HttpServlet {
             		while (iter.hasNext()) {
             			FileItemStream item = iter.next();
             			if (item.isFormField()) continue;
-            				storeInDataStore(item, req,resp);
+            				storeInSomeStore(user.getNickname(), item, req,resp);
             		}
             	} catch (FileUploadException e) {
+            		throw new ServletException("Cannot parse multipart request: " + e.getMessage());
+            	}catch (EntityNotFoundException e) {
             		throw new ServletException("Cannot parse multipart request: " + e.getMessage());
             	}
             	
@@ -87,30 +93,77 @@ public class Pa3jspServlet extends HttpServlet {
 
 	}
 	
-	public void storeInDataStore(FileItemStream item, 
+	public void storeInSomeStore(String userName,
+								 FileItemStream item, 
 								 HttpServletRequest req, 
-								 HttpServletResponse resp) throws IOException {
-		
-		InputStream stream = item.openStream();
+								 HttpServletResponse resp) throws IOException,
+								  EntityNotFoundException {
+
+		int contentLength;
+		int ONE_MB = 1024*1024;
 		
         resp.getWriter().println("<i> ops = " + "insert" + "</i><br>");
         resp.getWriter().println("<i> field_name = " + item.getFieldName() + "</i><br>");  
         resp.getWriter().println("<i> content-type = " + item.getContentType() + "</i><br>");  
         resp.getWriter().println("<i> is form field = " + item.isFormField() + "</i><br>");  
         resp.getWriter().println("<i> filename = " + item.getName() + "</i><br>");
-
+        resp.getWriter().println("<i> content-length = " + req.getContentLength() + "</i><br>");
+        
+        contentLength = req.getContentLength();
+        
+        if (contentLength < ONE_MB)
+        {
+        	storeInDataStore(userName, item, req, resp);
+        }
+        
+	 
+	}
+	
+	public void storeInDataStore(String userName,
+			 FileItemStream item, 
+			 HttpServletRequest req, 
+			 HttpServletResponse resp) throws IOException,
+			 								  EntityNotFoundException {	
+		InputStream stream = item.openStream();
         int len;
+        int chunk_count = 0;
         byte[] buffer = new byte[8192];
+        int complete_len = 0;
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        Key k = KeyFactory.createKey("user",  userName);
+       	Entity e =  datastore.get(k);
+       	Entity  f;
+       	
+       	Key k2 = new KeyFactory.Builder("user", userName).addChild("file", item.getName()).getKey();
+       	
+       	try {
+       	  f = datastore.get(k2);
+       	} catch (EntityNotFoundException ex) {
+       		  f = new Entity(k2);
+          	  resp.getWriter().println("<br><i>new file into datastore</i><br>" );       		  
+       	}
         String str = "";
         while ((len = stream.read(buffer, 0, buffer.length)) != -1) {
-            resp.getWriter().println("<br><i>Length Read=" + len + "</i><br>" );
-       	    str = "";
-       	    for(int i = 0; i < len; i++)
-            {
-                str += (char)buffer[i];
-            }
-       	    resp.getWriter().println(str);
-
-        }		 
+       	    //str = "";
+       	    //String content_property = "file-content-" + String.valueOf(chunk_count);
+       	    //String content_len_property = "file-contentlen-" + String.valueOf(chunk_count);
+       	    
+       	    //for(int i = 0; i < len; i++)
+            //{
+            //    str += (char)buffer[i];
+            //}
+            
+           	//f.setProperty(content_property, str);
+           	//f.setProperty(content_len_property, len);
+       	    complete_len += len;
+        	resp.getWriter().println("<br><i>File Chunk " + chunk_count +
+        			" with lenth " + len + 
+        			"Added as property of file entity." + "</i><br>" );
+        	chunk_count++;
+       	    
+        }	
+       	f.setProperty("file-contentlen", complete_len);
+       	datastore.put(f);
+        resp.getWriter().println("<br><i>File Length Added =" + complete_len + "</i><br>" );
 	}
 }
