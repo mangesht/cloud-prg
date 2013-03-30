539d19e121c8b384288026450a32606b1a370482
harsha.matadhikari <harsha.matadhikari@gmail.com>
2013-03-30 15:59:43 +0000

    update

--- prog3/src/cs553/pa3jsp/Pa3jspServlet.java
+++ prog3/src/cs553/pa3jsp/Pa3jspServlet.java
@@ -35,6 +35,13 @@ import com.google.appengine.api.datastore.EntityNotFoundException;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
 import com.google.appengine.api.datastore.KeyFactory.Builder;
+import com.google.appengine.api.datastore.Query.Filter;
+import com.google.appengine.api.datastore.Query.FilterPredicate;
+import com.google.appengine.api.datastore.Query.FilterOperator;
+import com.google.appengine.api.datastore.Query.CompositeFilter;
+import com.google.appengine.api.datastore.Query.CompositeFilterOperator;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.PreparedQuery;
 
 @SuppressWarnings("serial")
 public class Pa3jspServlet extends HttpServlet {
@@ -56,19 +63,25 @@ public class Pa3jspServlet extends HttpServlet {
             resp.getWriter().println("</div>");
             
             resp.getWriter().println("<div id=\"ops\">");
-            if (isMultipart == true) {
-            	try {
-            	performInsert(user, req,resp);
-            	} catch (IOException e) {
-            		throw new ServletException("Cannot parse multipart request: " + e.getMessage());
-            	}
-            } else {
-                	
-                String ops=req.getParameter("fun");
-            	String filename = req.getParameter("file_name");            		
-            	resp.getWriter().println("<i> ops = " + ops + "</i><br>");
-            	resp.getWriter().println("<i> filename = " + filename + "</i><br>");
-            }
+        	try {
+	            if (isMultipart == true) {
+	            	performInsert(user, req,resp);
+	            } else {
+	                	
+	                String ops=req.getParameter("fun");
+	                if (ops.equals("listing")) {
+	                	performListing(user, req,resp);
+	                } else if (ops.equals("check")) {
+	                	performCheck(user, req,resp);
+	                } else if (ops.equals("find")) {
+	                	performCheck(user, req,resp);
+	                } else if (ops.equals("remove")) {
+	                	performRemove(user, req,resp);
+	                }
+	            }
+        	} catch (IOException e) {
+        		throw new ServletException("Cannot parse multipart request: " + e.getMessage());
+        	}
             	    
             resp.getWriter().println("</div>");
        	    resp.getWriter().println("<div id=\"nav\">");
@@ -79,6 +92,93 @@ public class Pa3jspServlet extends HttpServlet {
         }
 
 	}
+	
+	public void performListing(User user,
+            HttpServletRequest req, 
+            HttpServletResponse resp)
+          		  throws IOException,
+                    ServletException         {
+		
+  	    String ops=req.getParameter("fun");		
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+       	Key k3 = new KeyFactory.Builder("user", user.getNickname()).getKey();  	    
+  	    Query q = new Query("fileinfo", k3);
+   	    PreparedQuery pq = datastore.prepare(q); 
+        resp.getWriter().println("<i> So you want see the listing of your files "
+						 + " ! Right? Here we go ... </i><br><br>");  
+        
+        resp.getWriter().println("<table border=1><tr><td>Filename</td><td>File Size in Bytes</td><td>File Store</td></tr> "); 
+   	    for (Entity result : pq.asIterable()) {  
+   	    	String filename = (String) result.getProperty("file-name");
+   	    	String filesize = String.valueOf(
+   	    	result.getProperty("file-contentlen"));
+   	    	String filestore = (String) result.getProperty("file-store");
+   	    	
+    	    resp.getWriter().println("<tr><td>" + filename +
+    							"</td><td>" + filesize + "</td>" + 
+    							"</td><td>" + filestore + "</td>" + 
+    	    					"</tr>");
+   	    }
+	    resp.getWriter().println("</table>");
+   	    
+	}
+
+	public void performFind(User user,
+            HttpServletRequest req, 
+            HttpServletResponse resp)
+          		  throws IOException,
+                    ServletException         {
+  	    String ops=req.getParameter("fun");		
+		String filename = req.getParameter("file_name");            		
+    	resp.getWriter().println("<i> ops = " + ops + "</i><br>");
+    	resp.getWriter().println("<i> filename = " + filename + "</i><br>");		
+	}
+	
+	public void performCheck(User user,
+            HttpServletRequest req, 
+            HttpServletResponse resp)
+          		  throws IOException,
+                    ServletException         {
+  	    String ops=req.getParameter("fun");		
+		String filename = req.getParameter("file_name");            		
+    	resp.getWriter().println("<i> ops = " + ops + "</i><br>");
+    	resp.getWriter().println("<i> filename = " + filename + "</i><br>");		
+	}	
+	
+	public void performRemove(User user,
+            HttpServletRequest req, 
+            HttpServletResponse resp)
+          		  throws IOException,
+                    ServletException         {
+  	    String ops=req.getParameter("fun");	
+  	    String filename=req.getParameter("file_name");	
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+       	Key k1 = new KeyFactory.Builder("user", user.getNickname())
+       						   .addChild("file", filename)
+       						   .getKey();  	    
+       	Key k2 = new KeyFactory.Builder("user", user.getNickname())
+		   .addChild("fileinfo", filename)
+		   .getKey();  	    
+       	
+  	    Query q = new Query(k2);
+   	    PreparedQuery pq = datastore.prepare(q); 
+   	    
+        resp.getWriter().println("<i> So you want to remove the file "
+						 + filename + " ! Right? Here we go ... </i><br><br>");  
+   	    for (Entity result : pq.asIterable()) {  
+   	    	String filesize = String.valueOf(
+   	    	    	result.getProperty("file-contentlen"));
+   	    	
+   	    	if (Integer.valueOf(filesize) > 1024*924)	{
+   	    		resp.getWriter().println("<i> File is stored in Blobstore, deleting it from there </i><br>");
+   	    	} else {
+   	    		resp.getWriter().println("<i> File is stored in Datastore, deleting it from there </i><br>");
+   	    		datastore.delete(k1);
+   	    	}
+   	    }
+   	    
+        datastore.delete(k2);
+	}	
 	
 	public void performInsert(User user,
 			                  HttpServletRequest req, 
@@ -98,7 +198,7 @@ public class Pa3jspServlet extends HttpServlet {
     		}
     	} catch (FileUploadException e) {
     		throw new ServletException("Cannot parse multipart request: " + e.getMessage());
-    	}catch (EntityNotFoundException e) {
+    	} catch (EntityNotFoundException e) {
     		throw new ServletException("Cannot parse multipart request: " + e.getMessage());
     	}
     	
@@ -165,17 +265,6 @@ public class Pa3jspServlet extends HttpServlet {
        	
         String str = "";
         while ((len = stream.read(buffer, complete_len, (buffer.length-complete_len))) != -1) {
-       	    //str = "";
-       	    //String content_property = "file-content-" + String.valueOf(chunk_count);
-       	    //String content_len_property = "file-contentlen-" + String.valueOf(chunk_count);
-       	    
-       	    //for(int i = 0; i < len; i++)
-            //{
-            //    str += (char)buffer[i];
-            //}
-            
-           	//f.setProperty(content_property, str);
-           	//f.setProperty(content_len_property, len);
        	    complete_len += len;
         	resp.getWriter().println("<br><i>File Chunk " + chunk_count +
         			" with lenth " + len + "</i><br>" );
@@ -188,8 +277,9 @@ public class Pa3jspServlet extends HttpServlet {
        	f.setProperty("file-content", blob);
        	d.setProperty("file-contentlen", complete_len);
        	d.setProperty("file-name", item.getName());
+       	d.setProperty("file-store", "DataStore");       	
        	datastore.put(f);
        	datastore.put(d);       	
-        resp.getWriter().println("<br><i>File " +  item.getName() + "Added. Length =" + complete_len + "</i><br>" );
+        resp.getWriter().println("<br><i>File " +  item.getName() + " Added. Length =" + complete_len + "</i><br>" );
 	}
 }
