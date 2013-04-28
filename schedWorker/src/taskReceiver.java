2e813e89f333a808fa89f5a2147dc8093bf16e7e
harsha.matadhikari@gmail.com <harsha.matadhikari@gmail.com@837e750e-98ea-f7b5-08b2-a96902323fd1>
2013-04-28 10:39:06 +0000

    updated

--- schedWorker/src/taskReceiver.java
+++ schedWorker/src/taskReceiver.java
@@ -1,33 +1,32 @@
-import java.io.*;
 import java.net.*;
-import java.util.concurrent.BlockingQueue;
+
 public class taskReceiver extends Thread {
 	public commonInfo cInfo; 
 	   
-	   static String taskRequestXML;
-	   static String responseXML;
-	   static DatagramPacket receivePacket;
-	   static DatagramPacket sendPacket;
-	   	   
-	   public void receiveRequestXML() {
-			  byte[] receiveData = new byte[1024];
-			  try {
-				  System.out.println("Server waiting for task ");
-				  receivePacket = new DatagramPacket(receiveData, receiveData.length);
-				  cInfo.serverSocket.receive(receivePacket);
-				  taskRequestXML = new String( receivePacket.getData());
-				  cInfo.IPAddress = receivePacket.getAddress();
-				  cInfo.port = receivePacket.getPort();
-				  taskRequestXML = taskRequestXML.trim();
-				  //taskRequestXML = "<request><task><taskid>" +"1" + "</taskid><taskstr>sleep "+ "1000" +" </taskstr></task></request>";
-				  System.out.println("RECEIVED: Length "+ taskRequestXML.length() + taskRequestXML); 
-				  cInfo.taskQ.put(taskRequestXML);
-			      
-			  } catch (Exception error) {
-				  System.err.println("Task Receiver : Error in socket communication " + error.getMessage());
-				  return ;
-			  }
-		   }
+   static String taskRequestXML;
+   static String responseXML;
+   static DatagramPacket receivePacket;
+   static DatagramPacket sendPacket;
+   	
+   
+   public void receiveRequestXML() {
+		  byte[] receiveData = new byte[1024];
+		  try {
+			  System.out.println("Server waiting for task ");
+			  receivePacket = new DatagramPacket(receiveData, receiveData.length);
+			  cInfo.serverSocket.receive(receivePacket);
+			  taskRequestXML = new String( receivePacket.getData());
+			  cInfo.IPAddress = receivePacket.getAddress();
+			  cInfo.port = receivePacket.getPort();
+			  taskRequestXML = taskRequestXML.trim();
+			  System.out.println("RECEIVED: Length "+ taskRequestXML.length() + taskRequestXML); 
+			  cInfo.taskQ.put(taskRequestXML);
+		      
+		  } catch (Exception error) {
+			  System.err.println("Task Receiver : Error in socket communication " + error.getMessage());
+			  return ;
+		  }
+	   }
 	   
 	public void run(){
 		while (true){
