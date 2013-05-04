e0ab2bef3d9025ec18f6b79bcd50e4b5fe96e551
mangesh.thakare@gmail.com <mangesh.thakare@gmail.com@837e750e-98ea-f7b5-08b2-a96902323fd1>
2013-05-04 09:25:54 +0000

    Updated for parsing

--- remoteWorker/src/remoteWorker.java
+++ remoteWorker/src/remoteWorker.java
@@ -31,6 +31,7 @@ public class remoteWorker {
 	 * @param args
 	 */
 	static commonInfo cInfo;
+	static String taskResponseXML="";
 
 	public static void remoteSend(String message) {
 		try { 
@@ -59,13 +60,124 @@ public class remoteWorker {
 		sc = sc.trim(); 
 		System.out.println("Sleep Time = " + Integer.valueOf(sc) ); 
 		return Integer.valueOf(sc);  
-
 	}
+	
+	public static void parseRequest(String xmlRequest)
+	{
+	   xmlRequest = xmlRequest.trim();
+		try {
+			DocumentBuilderFactory fact1 = DocumentBuilderFactory.newInstance();
+			fact1.setValidating(false);
+			fact1.setIgnoringElementContentWhitespace(true);
+			DocumentBuilder build1 = fact1.newDocumentBuilder();
+
+			ByteArrayInputStream astream = 
+						new ByteArrayInputStream(xmlRequest.getBytes());
+			Document requestDoc = build1.parse(new BufferedInputStream(astream));
+			Element requestElement =  requestDoc.getDocumentElement();
+			NodeList taskBlockNode = requestElement.getChildNodes();
+			taskResponseXML = "<response>";
+			for (int i = 0; i < taskBlockNode.getLength(); i++) {
+				Element taskBlock  = (Element) taskBlockNode.item(i);
+				taskResponseXML += processCompleteBlock(taskBlock);
+			}
+			taskResponseXML += "</response>";	
+		}
+		catch (Exception error) {
+			System.err.println("Error parsing : " + error.getMessage());
+		}		
+	}   
+	public static String  processCompleteBlock(Element taskBlock) {
+		String responseXML = "";
+		NodeList tasks  = taskBlock.getChildNodes();
+		responseXML += "<taskblock>"; 
+		for (int j = 0; j < tasks.getLength(); j++) {
+			Element task  = (Element) tasks.item(j);
+			boolean result;
+			
+			responseXML += "<task>";					
+			NodeList taskId  = task.getElementsByTagName("taskid");
+			
+			Text txtTaskId = (Text) taskId.item(0).getFirstChild();
+			responseXML += "<taskid>" + txtTaskId.getData() + "</taskid>";					
+			//System.out.println(responseXML);
+			NodeList taskStrNode  = task.getElementsByTagName("taskstr");
+			
+			Text txtTaskStr = (Text) taskStrNode.item(0).getFirstChild();
+
+			
+			responseXML += "<taskstr>" + txtTaskStr.getData() + "</taskstr>";
+			//System.out.println(responseXML);			
+			result = executeTask(txtTaskId, txtTaskStr);		
+			if (result == true) {
+				responseXML += "<taskstatus>" + "success" + "</taskstatus>";
+			} else {
+				responseXML += "<taskstatus>" + "failure" + "</taskstatus>";
+			}
+			responseXML += "</task>";	
+		}
+		responseXML += "</taskblock>";	
+		return responseXML;
+	}
+	
+	public static boolean executeTask(Text txtTaskId, Text txtTaskStr) {
+		// Currently only sleep task is accepted as per the project
+		   long sleepTime =  getSleepTime(txtTaskStr.getData()); 
+		   System.out.println("localworker " + 
+				   					" sleepTime = " + sleepTime +
+				   					" taskid = " + txtTaskId.getData());
+
+			try {
+				Thread.sleep(sleepTime);
+				return true;
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return false;
+			}
+	}
+	static void  displayHelp(){ 
+		System.out.println("");
+		System.out.println("Usage p4client [-h] -i Timeout");
+	}
+    public static boolean parseArgs(String[] args) {
+		String str;    	
+		// Input Parsing 
+		int argsLen = 0;
+		int idx=0;		
+		argsLen = args.length ;
+		
+		cInfo = new commonInfo();
+	
+		while(idx < argsLen ) {
+			str = args[idx];
+			if(str.charAt(0) == '-'){
+				 if(str.charAt(1) == 'h'){
+					 //help
+					 displayHelp();
+
+				 }else if(str.charAt(1) == 'i'){
+					 // Timeout 
+					 cInfo.timeout  = Integer.parseInt(args[idx+1]) ;                                                 
+					 idx++;
+				 }
+				 idx++;
+			 }
+			 else {
+				 idx++;
+			 }
+		}
+		
+
+		return true;
+    }
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		String res;
 		cInfo = new commonInfo();
-		watchDogTimer wDogT; 
+		parseArgs(args);
+		watchDogTimer wDogT;
+		
 		SQSInit();
 
 		int i;
@@ -74,25 +186,17 @@ public class remoteWorker {
 		String response = "";
 		long sleepTime ;
 		wDogT = new watchDogTimer(); 
+		wDogT.cInfo = cInfo;
+		System.out.println("Started remote worker with Timeout " + cInfo.timeout );
 		// Start watchDog Thread
 		if(cInfo.timeout != 0 ) { 
 			wDogT.start();
 		}
-		DocumentBuilderFactory fact1 = DocumentBuilderFactory.newInstance();
-		DocumentBuilder build1 = null ; // = fact1.newDocumentBuilder()
 
-		fact1.setValidating(true);
-		fact1.setIgnoringElementContentWhitespace(true);
-		System.out.println("Worker " +   " started");
-		try {
-			build1 = fact1.newDocumentBuilder();
-		} catch (ParserConfigurationException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
 		while(cInfo.die == false ) { 
 			cInfo.idle = true ; 
 			List<Message> messages = null ;
+			System.out.println("Try to get message from " + cInfo.taskQueueUrl ); 
 			ReceiveMessageRequest receiveMessageRequest = new ReceiveMessageRequest(cInfo.taskQueueUrl);
 			receiveMessageRequest .setMaxNumberOfMessages(1);
 			//Recursively try to get the job 
@@ -126,39 +230,17 @@ public class remoteWorker {
 				res = message.getBody();
 
 				System.out.println("Worker = "  + " Received task : Len " + res.length() +" Task Name " + res); 
-				try {
-					ByteArrayInputStream astream = 
-							new ByteArrayInputStream(res.getBytes());
-					Document requestDoc = build1.parse(new BufferedInputStream(astream));
-					Element requestElement =  requestDoc.getDocumentElement();
 
-					NodeList taskNode = requestElement.getChildNodes();
-					for ( i = 0; i < taskNode.getLength(); i++) {
-						Element task  = (Element) taskNode.item(i);
-						NodeList taskId  = task.getElementsByTagName("taskid");
-						txtTaskId = (Text) taskId.item(0).getFirstChild();
-						NodeList taskStr  = task.getElementsByTagName("taskstr");
-						txtTaskStr = (Text) taskStr.item(0).getFirstChild();
-						System.out.println("task id = " + txtTaskId.getData() );
-						System.out.println("task str = " + txtTaskStr.getData() );
-					}
-
-				}
-				catch (Exception error) {
-					System.err.println("Error parsing : " + error.getMessage());
-				}
-
-				sleepTime =  getSleepTime(txtTaskStr.getData()); 
-				System.out.println(" Actual sleep task here");
+				
 				cInfo.idle = false ;
-				// Actual sleep task here 
-
-				Thread.sleep(sleepTime);
+				// Parse the message and Actual sleep task here 
+				parseRequest(res);
+				
 
 				// Job Done Send result to scheduler
-				response = "<response><task><taskid>" + txtTaskId.getData() + "</taskid><taskstr>" + txtTaskStr.getData() + "</taskstr><taskresult>1</taskresult></task></response>";
-				System.out.println(" "  +response);
-				remoteSend(response);
+				//response = "<response><task><taskid>" + txtTaskId.getData() + "</taskid><taskstr>" + txtTaskStr.getData() + "</taskstr><taskresult>1</taskresult></task></response>";
+				System.out.println(" "  +taskResponseXML);
+				remoteSend(taskResponseXML);
 				
 	            // Delete the processed message message
 	            System.out.println("Deleting a message.\n");
