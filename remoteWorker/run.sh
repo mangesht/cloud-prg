a5d8a8f9a423b3bae3d16d4e7d25b7eed691dc3d
mangesh.thakare@gmail.com <mangesh.thakare@gmail.com@837e750e-98ea-f7b5-08b2-a96902323fd1>
2013-05-05 08:38:04 +0000

    Updated for AMI

--- remoteWorker/run.sh
+++ remoteWorker/run.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 touch /home/ubuntu/mangesht.txt
-sleep 30
+sleep 1
 cd /home/ubuntu/remoteWorker
-svn up
+svn up src
 make clean
 make run OPT="-i 100" >  ~/log1.log 
-sudo shutdown now
+sudo shutdown 1
 
