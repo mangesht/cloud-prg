e711a706c87ee2fab8c2ad2147b37dadf1d25061
mangesh.thakare@gmail.com <mangesh.thakare@gmail.com@837e750e-98ea-f7b5-08b2-a96902323fd1>
2013-05-08 04:20:21 +0000

    client updated with END

--- schedWorker/src/client.java
+++ schedWorker/src/client.java
@@ -259,24 +259,10 @@ public class client {
   	      //clientTCPSocket.setSendBufferSize(size);
 		  inputSockStream = clientTCPSocket.getInputStream();
 		  outputSockStream = clientTCPSocket.getOutputStream();
-		  int beginIndex = 0 ;
-		  int endIndex = 0 ; 
-		  int maxSize = xmlRequest.length ();
-		  String str; 
-		  endIndex = 100000; 
-		 System.out.println("Input LENGTH=" + xmlRequest.length() + " DATA={" + xmlRequest  + "}" );
-		  endIndex = maxSize <= endIndex ? maxSize -1  : endIndex ;  
-		  while(beginIndex < endIndex ){ 
-			  System.out.println("Begin Index " + beginIndex + " endIndex " + endIndex + "Max Size " + maxSize ) ;  
-			  str = xmlRequest.substring(beginIndex, endIndex);
-			  beginIndex = endIndex ; 
-			  endIndex = beginIndex + 100000; 
-		          endIndex = maxSize <= endIndex ? maxSize -1  : endIndex ;  
-			  //outputSockStream.write(xmlRequest.getBytes());
-			  outputSockStream.write(str.getBytes());
-			  System.out.println("Buffer Size = " + clientTCPSocket.getSendBufferSize());
-			  System.out.println("SENTXMLFILE LENGTH=" + str.length() + " DATA={" + str  + "}" );
-		  }
+		  xmlRequest += "END"; 
+		  outputSockStream.write(xmlRequest.getBytes());
+		  System.out.println("Buffer Size = " + clientTCPSocket.getSendBufferSize());
+		  System.out.println("SENTXMLFILE LENGTH=" + xmlRequest.length() + " DATA={" + xmlRequest + "}" );
 	    }
 	    catch (Exception error){
 		  System.err.println("Error in socket communication " + error.getMessage());
